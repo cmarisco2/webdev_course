752384bf463579cb8df2a40ed19bfbfa5cb2fdf2
Christopher Marisco <christopher.marisco@gmail.com>
2021-11-12 20:13:12 -0700

    practice using element selectors of the JS DOM

--- 2021_Portfolio_Builder/DOM_Play/DOM_Intro_CODE/Selecting/app.js
+++ 2021_Portfolio_Builder/DOM_Play/DOM_Intro_CODE/Selecting/app.js
@@ -1,18 +1,20 @@
 const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+for(let image of allImages){
+    // console.log(image.src);
+    // image.src = 'https://images.unsplash.com/photo-1548550023-2bdb3c5beed7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
 }
-
 
 const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+for(let img of squareImages){
+    // img.src ='https://images.unsplash.com/photo-1548550023-2bdb3c5beed7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+    console.log(img.src);
 }
 
-const links = document.querySelectorAll('p a');
+const newImages = document.querySelectorAll('img');
 
-for (let link of links) {
-    console.log(link.href)
+for(let img of newImages){
+    img.src = 'https://images.unsplash.com/photo-1575540325855-4b5d285a3845?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80';
 }
+
