694d4a0efe00e041726d7cf71e39eb3cc2c05931
Christopher Marisco <christopher.marisco@gmail.com>
2021-11-26 12:13:17 -0700

    finish creating servers demo with express to include pattern matching routes and utilizing the query string in express

--- 2021_Portfolio_Builder/Express_Play/firstApp/index.js
+++ 2021_Portfolio_Builder/Express_Play/firstApp/index.js
@@ -2,6 +2,63 @@ const express = require('express');
 // Factory Function?
 const app = express();
 
+/**
+ * app.use() replies to anything.
+ * once res.send() is reached, the function ends. Like a 'return' statement.
+ */
+// app.use((req, res) => {
+//     console.log("Getting use on this port");
+//     // res.send("Request Acknowledged");
+//     res.send({color: 'red'});
+// });
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Rapture');
+});
+
+app.get('/bioshock', (req, res) => {
+    res.send('Would You Kindly?');
+});
+
+app.get('/bioshock2', (req, res) => {
+    res.send('Would You Kindly?');
+});
+
+app.get('/bioshockInfinite', (req, res) => {
+    res.send('Constants and Variables');
+});
+
+/**
+ * The colon in the path allows for variables that follow the pattern.
+ * like "/r/MightyHarvest"
+ * 
+ * Must Start path with '/' and the pattern looks for further '/' too.
+ */
+
+//req.params will have the actual value of /:subreddit stored in an object.
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.send(`Browsing the ${subreddit} subreddit`);
+});
+/**
+ * Query String Found via req.query
+ */
+
+app.get('/search', (req, res) => {
+    res.send(req.query);
+})
+/**
+ * Can use a wildcard "*" for anything else that's not explicitly listed.
+ * 
+ * Put last because routes are matched in order
+ */
+
+app.get('*', (req, res) => {
+    res.send("Sorry, I don't recognize this route");
+});
+
 app.listen(3000, () => {
     console.log('listening on port 3000');
-})
+});
+
+//To auto-restart express after save: install nodemon globally with sudo permissions.
