6e3f856506becd84582f07f46bc307ceadfe3d70
Christopher Marisco <christopher.marisco@gmail.com>
2021-11-25 19:47:53 -0700

    export data in an object on the export module js file using module.exports = {'[data]'}. import (and destructure if desired) the exported module using const var1 = require('[filePath/ModuleName]').

---
+++ 2021_Portfolio_Builder/Node_Play/module_play/module_data_exports.js
@@ -0,0 +1,7 @@
+const favoritNumber = 2;
+
+const printMyFav = (value) => {
+    console.log(`My Favorite is ${value}`);
+}
+// SENDS functions and data to be exported.
+module.exports = {favoritNumber, printMyFav};
