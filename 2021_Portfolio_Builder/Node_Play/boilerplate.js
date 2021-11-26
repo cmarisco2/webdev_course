bdf5a4c4655ec53abaf8df7142e43f147380917f
Christopher Marisco <christopher.marisco@gmail.com>
2021-11-25 19:31:08 -0700

    create script for auto generating folders with index.htm app.cs and app.js files via the fs module in nodeJS

--- 2021_Portfolio_Builder/Node_Play/boilerplate.js
+++ 2021_Portfolio_Builder/Node_Play/boilerplate.js
@@ -1,9 +1,12 @@
 // Requires the fs module
 const fs = require('fs');
-const folderName = process.argv.slice(2);
+const folderName = process.argv[2].toString() || 'Project';
 
-fs.mkdirSync(folderName);
-
-fs.writeFileSync(`${folderName}/index.html`);
-fs.writeFileSync(`${folderName}/styles.css`);
-fs.writeFileSync(`${folderName}/app.js`);
+try {
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`, '');
+    fs.writeFileSync(`${folderName}/styles.css`, '');
+    fs.writeFileSync(`${folderName}/app.js`, '');
+} catch(e) {
+    console.log("AN ERROR OCCURED: ", e);
+}
