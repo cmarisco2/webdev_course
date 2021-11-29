4567e6467030f9c2eab3fd420f4e9045af4aa252
Christopher Marisco <christopher.marisco@gmail.com>
2021-11-29 13:36:21 -0700

    update index.js for patch request. update edit.ejs to have query string at the end of the action form to override the post request for patching

--- 2021_Portfolio_Builder/Express_Play/REST_Play/CRUD_NoDB/index.js
+++ 2021_Portfolio_Builder/Express_Play/REST_Play/CRUD_NoDB/index.js
@@ -21,7 +21,7 @@
  * Post - /comments - create a new comment
  ** READ Single Comment: 'Show Route'
  * Get - /comments/:id
- ** Update Single Comment:
+ ** Update Single Comment: 'Patch'
  * Patch - /comments/:id
  ** Edit Single Comment: (request Form to retrieve comment to patch)
  * GET - /comments/:id/edit
@@ -34,8 +34,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4:uuid } = require('uuid');
+//! Allows overriding and HTTP Method on a form
+const methodOverride = require('method-override');
+
 
 app.use(express.urlencoded({extended: true}));
+//! Allows overriding and HTTP Method on a form
+//! Add as a query string to the end of the action address of the form (Patch Route)
+//! --ex: action=":patchAddres?_method=PATCH" method ="POST"
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -43,22 +51,22 @@ app.set('views', path.join(__dirname, 'views'));
 //! Array of Objects as a pseudo database of comments.
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Eren Jaegar',
         comment: 'I just want to be free' 
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Booker DeWitt',
         comment: 'Give us the girl, and wipe away the debt'
     },
     {
-        id: 3,
+        id: uuid(),
         username: 'Sans',
         comment: 'LoloLolOloL'
     },
     {
-        id: 4, 
+        id: uuid(), 
         username: 'Dwayne Johnson',
         comment: 'If ya SMEELLLLLLLL!!! What the ROCK IS COOKIN'
     }
@@ -76,25 +84,49 @@ app.get('/comments', (req, res) => {
     res.render('comments/index', {comments});
 });
 
-// Show Single Comment
-// 1 - retrieve comment from params id
-// 2 - render ejs page with the comment
-app.get('/comments/:id', (req, res) => {
-    const { id } = req.params;
-    const commentObject = comments.find( c => c.id === parseInt(id));
-    const {username, comment} = commentObject;
-    res.render('comments/show', {username, comment, id});
-});
 
 //Create(GET) New Form & Submit(Post) New Form:
 //Ensure to REDIRECT in the POST to not resubmit the form!
+
 app.get('/comments/new', (req, res) => {
     res.render('comments/new');
 });
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({username, comment});
+    comments.push({username, comment, id: uuid()});
     res.redirect('/comments');
+});
+
+// Update a comment -> Patch Request
+// * Patch vs Put: Patch (updates what's there) Put (replaces the whole thing)
+// ? NEED to Modify Form (obtained via Get request) to handle a Patch Method
+// ? Pre-populate the form via finding the comment and rendering it in the form.
+//!Need to install method-override package for express to patch via html form.
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newComment = req.body.comment; //* 'comment' can be simulated via Postman.
+    const commentObject = comments.find( c => c.id === id);
+    commentObject.comment = newComment;
+    res.redirect('/comments');
+
+});
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const commentObject = comments.find( c => c.id === id);
+    const { username, comment } = commentObject;
+    res.render('comments/edit', {comment, username, id});
+});
+
+//!Make sure pattern matching addresses are put AFTER explicit ones!
+//! -ex: /comments/new will not be reached, unless it's BEFORE /comments/:id
+// Show Single Comment
+// 1 - retrieve comment from params id
+// 2 - render ejs page with the comment
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const commentObject = comments.find( c => c.id === id);
+    const {username, comment} = commentObject;
+    res.render('comments/show', {username, comment, id});
 });
 
 
