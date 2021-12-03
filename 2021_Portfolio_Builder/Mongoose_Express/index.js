7007eda482dbffcdb9be30c911b5b696867f06cb
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-03 16:15:18 -0700

    create project to connect mongoose and express lessons. install dependencies path, express, mongoose, and ejs. setup express test route and connect to mongo db via mongoose

---
+++ 2021_Portfolio_Builder/Mongoose_Express/index.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ ** "Putting It All Together" 
+ * 
+ * 
+ ** Basic Setup in Section 38 vid 396 for reference 
+ */
+//! Setup - Start
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/farmStand');
+}
+main()
+    .then(() => console.log("Connected To Mongo"))
+    .catch(err => console.log("Connection Error", err));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//! Setup - End
+
+
+//?Routes:
+app.get('/dog', (req, res) => {
+    res.send('Connected On the Dogs Route');
+});
+
+
+
+//? Listening
+app.listen(3000, () => {
+    console.log("App is Listening on Port 3000");
+});
