f982120d2ab637b0df0f18fc7247257e8c6a0e1d
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-03 21:27:09 -0700

    add CRUD methods to index.js for accessing the database model, use npm to install dependency for method-override (and add it to package.json), add views for edit.ejs, show.ejs, new.ejs, and index.ejs for all the different views of the Product Schema's CRUD interface

--- 2021_Portfolio_Builder/Mongoose_Express/index.js
+++ 2021_Portfolio_Builder/Mongoose_Express/index.js
@@ -3,7 +3,11 @@
  ** "Putting It All Together" 
  * 
  * 
- ** Basic Setup in Section 38 vid 396 for reference 
+ ** Basic Setup in Section 38 vid 396 for reference + method-override & middleware
+ *
+ ** Add CRUD (connects all database/webserver/templating options)
+ *
+ *
  */
 //! Setup - Start
 const express = require('express');
@@ -11,6 +15,7 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Product = require('./models/product');
+const methodOverride = require('method-override');
 
 
 async function main() {
@@ -22,13 +27,62 @@ main()
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 //! Setup - End
 
 
 //?Routes:
-app.get('/dog', (req, res) => {
-    res.send('Connected On the Dogs Route');
+//! Note: Data From the Product Model & DB (req from mongoose asynchronously OR then/catch)
+//* async and await:
+//* INDEX
+app.get('/products', async (req, res) => {
+    const products = await Product.find({}); //*array of product objects from DB
+    res.render('products/index', { products });
 });
+
+//* CREATE: 1) GET FORM 2) POST FORM (NEED TO USE PARSE MIDDLEWARE FOR POSTING)
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+});
+app.post('/products', async (req, res) => {
+    const {name, price, fruit, category} = req.body;
+    const newProduct = new Product({name, price, fruit, category});
+    await newProduct.save();
+    res.redirect('/products');
+});
+
+
+//* SHOW/DETAILS
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const foundProduct = await Product.findById(id);
+    res.render('products/show', {foundProduct});
+});
+
+//* UPDATE: 1) GET FORM w/Data Filled Out 2) Change/PUTorPATCH/Redirect
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const foundProduct = await Product.findById(id);
+    res.render('products/edit', {foundProduct});
+});
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const foundProduct = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${foundProduct._id}`);
+});
+
+//* DELETE: -add 'form' to SHOW page and override its action. delete from db and redirect
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+});
+
+
+
 
 
 
