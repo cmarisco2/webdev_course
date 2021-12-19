de72f07c9ac8672cf5cb4b692eb3fb630164ff89
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-19 15:51:01 -0700

    add mongoose middleware .post() for the 'findOneAndDelete' trigger function in the yelp-camp campground.js model schema. To this middleware, capture the deleted doc. import reviews model to campground.js. Within middleware, remove from Reviews object any review contained in the deleted doc

--- 2021_Portfolio_Builder/Yelp_Camp/models/campground.js
+++ 2021_Portfolio_Builder/Yelp_Camp/models/campground.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //alias
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 
 const campgroundSchema = new Schema({
@@ -15,6 +16,22 @@ const campgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+});
+
+//!Mongoose middleware REQUIRES analyzing docs just to hit correct middleware
+//* Post middleware:
+//* 1 - has access to the deleted campground as arg 'doc'
+//* 2 - called on 'findOneAndDelete' though triggering function is 'findByIdAndDelete' -> AGAIN READ THE RED COMMENT ABOVE
+//* 3 - can examine deleted doc --> Delete Refs in Separate Doc (campgrounds -> reviews)
+//! 4 - No Need to change original delete campground route! just add middleware to mutate the deleted object!
+campgroundSchema.post('findOneAndDelete', async function(doc){
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        });
+    }
 });
 
 module.exports = mongoose.model('Campground', campgroundSchema);
