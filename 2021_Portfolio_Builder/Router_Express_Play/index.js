9eb8bc087cb4d707e4c09b97aca8b92040878f5d
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-19 17:36:31 -0700

    require each routes module in the index.js file. ex: const shelterRoutes = require('./routes/shelters.js'); use express middleware to add prefix to each imported route and to use the routes imported as well. ex: app.use('/shelters', shelterRoutes);

--- 2021_Portfolio_Builder/Router_Express_Play/index.js
+++ 2021_Portfolio_Builder/Router_Express_Play/index.js
@@ -1,5 +1,28 @@
+/**
+ ** Express Router Demo - Index.js (route hub js file)
+ *
+ ** 1 -> shelterRoutes = require('./routes/shelters');
+ ** 2 -> app.use('/shelters', shelterRoutes) 
+ */
+
+/**
+ ** Express Router Demo - Individual Route Files
+ *
+ ** 1 -> router = express.Router();
+ ** 2 -> make paths BUT use 'router.get()' vice 'app.get()' etc.
+ ** 3 -> export the routes 
+ */
 const express = require('express');
 const app = express();
+const shelterRoutes = require('./routes/shelters');
+const dogRoutes = require('./routes/dogs');
+
+//! Middleware that runs on every '/shelters' route
+//! Adds '/shelters' as a prefix to the ROUTER from shelterRoutes
+//? ---therefore, can remove the '/shelters' prefix in the shelter.js file
+app.use('/shelters', shelterRoutes);
+app.use('/dogs', dogRoutes);
+
 
 
 app.listen(3000, () => {
