258a92d6a837dca12da79bf24617e5c276210241
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-20 15:16:47 -0700

    require express-session module, add middleware for using a session w/ signature. make demo property to add on req.session object to increment upon visiting the same route from the same browser. All data store storage is temporarily retained in the browser's memory

--- 2021_Portfolio_Builder/Session_Play/index.js
+++ 2021_Portfolio_Builder/Session_Play/index.js
@@ -1,8 +1,23 @@
+/**
+ ** Demo: Sessions 
+ *  
+ */
 const express = require('express');
 const app = express();
 
+//* Require module, use middleware:
+const session = require('express-session');
+app.use(session({secret: 'secretKey'}));
 
 
+app.get('/viewcount', (req, res) => {
+    if(req.session.count){
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`Your View Count, Sir? \n It's ${req.session.count}`);
+});
 
 
 app.listen(3000, () => {
