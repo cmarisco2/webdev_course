f1f7e12f3c88b9dbb9ccad5fa2a605c822529ffd
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-20 11:57:11 -0700

    require cookie-parser module and run in middleware. use res.cookie and req.cookies to send and retrieve cookies in demo routes

--- 2021_Portfolio_Builder/Cookies_Play/index.js
+++ 2021_Portfolio_Builder/Cookies_Play/index.js
@@ -1,10 +1,29 @@
+/**
+ *? Application to Demo Sending Cookies
+ */
 const express = require('express');
 const app = express();
 
+//?Needed for parsing cookies:
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 
 
+//* Send Cookies:
+//* use "res.cookie" to set a key-value pair cookie
+app.get('/setName', (req, res) => {
+    //! Sending Cookie
+    res.cookie('name', 'Sephiroth');
+    res.send("Sent You a Cookie, Chum!");
+});
 
+//* Retrieve Cookies:
+//* use req.cookie -> typically, destructure for parsing
+app.get('/greet', (req, res) => {
+    const { name = "Anonymous" } = req.cookies;
+    res.send(`Hello ${ name }`);
+});
 
 
 app.listen(3000, () => {
