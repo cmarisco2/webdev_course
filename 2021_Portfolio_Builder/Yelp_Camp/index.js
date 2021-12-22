42333ca4d5cddbb4163c497e888c44e77e120ddb
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-22 14:18:14 -0700

    remove campgrounds routes and shift to separate campgrounds.js file within routes/

--- 2021_Portfolio_Builder/Yelp_Camp/index.js
+++ 2021_Portfolio_Builder/Yelp_Camp/index.js
@@ -9,6 +9,7 @@ const { findByIdAndUpdate } = require('./models/campground');
 const ExpressError = require('./utilities/ExpressError');
 const catchAsync = require('./utilities/catchAsync');
 const { campgroundSchema, reviewSchema } = require('./validationSchemas/schemas');
+const campgroundRoutes = require('./routes/campgrounds');
 
 const Review = require('./models/review');
 
@@ -51,59 +52,15 @@ const validateReview = (req, res, next) => {
 }
 
 //?ROUTES:
+
+//? Camprounds Router:
+app.use('/campgrounds', campgroundRoutes);
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-//* INDEX
-app.get('/campgrounds', catchAsync(async(req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}));
 
-
-//* CREATE
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-});
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
-    //? Need to use middleware to know how to parse 'req.body'
-    //? Note: req.body.campground is an object => constructor doesn't require '{}'
-    //* We Know that campground is posted in the body from 'new' form
-
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-//* SHOW
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews'); //* Populate Reviews
-    res.render('campgrounds/show', { campground });
-}));
-
-//* EDIT
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', { campground });
-}));
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    //INCORRECT -> Need to update NOT CREATE
-    // const campground = new Campground(req.body.campground);
-    // await campground.save();
-    //?Get id from params. new data in the req.body. use spread operator to send as 2nd arg to findByIdAndUpdate
-    const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-//* DELETE -> sent via form w/button, overrided method, on existing form page (like SHOW)
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
 
 //? Reviews Routes:
 //* CREATE 1) "GET" route is actually just Show/Details of a Campround w/ Form
