0df07e130d186b745b547642ef1232ebda5776c3
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-22 14:17:09 -0700

    create campgrounds.js in routes/ and move over campgrounds routes from index.js, as well as dependenies, to campgrounds.js file. fix file path issues within dependencies

---
+++ 2021_Portfolio_Builder/Yelp_Camp/routes/campgrounds.js
@@ -0,0 +1,74 @@
+/**
+ ** Move Campground Routes, dependencies, and middleware funtions from index.js to this file 
+ */
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utilities/catchAsync');
+const ExpressError = require('../utilities/ExpressError');
+const Campground = require('../models/campground');
+const { campgroundSchema } = require('../validationSchemas/schemas');
+
+//* Middleware for validating campgrounds. Add as argument to desired routes. next()
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(400, msg);
+    } else {
+        next();
+    }
+};
+
+
+//? Campgrouds:
+//* INDEX
+router.get('/', catchAsync(async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}));
+
+
+//* CREATE
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+router.post('/', validateCampground, catchAsync(async (req, res) => {
+    //? Need to use middleware to know how to parse 'req.body'
+    //? Note: req.body.campground is an object => constructor doesn't require '{}'
+    //* We Know that campground is posted in the body from 'new' form
+
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+//* SHOW
+router.get('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate('reviews'); //* Populate Reviews
+    res.render('campgrounds/show', { campground });
+}));
+
+//* EDIT
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+}));
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+    //INCORRECT -> Need to update NOT CREATE
+    // const campground = new Campground(req.body.campground);
+    // await campground.save();
+    //?Get id from params. new data in the req.body. use spread operator to send as 2nd arg to findByIdAndUpdate
+    const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+//* DELETE -> sent via form w/button, overrided method, on existing form page (like SHOW)
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+}));
+
+module.exports = router;
