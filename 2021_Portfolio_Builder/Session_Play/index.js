44aa0aab1ebb25f588074955df6105ad24c8fdc1
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-22 13:07:59 -0700

    use sessions object by adding properties to itfrom the client's data (query string, site visits, etc) and displaying the output to the client. client stores only cookie that retrieves sessions data store.

--- 2021_Portfolio_Builder/Session_Play/index.js
+++ 2021_Portfolio_Builder/Session_Play/index.js
@@ -1,5 +1,6 @@
 /**
- ** Demo: Sessions 
+ ** Demo: Sessions.
+ ** Require module, use middleware, pass in options
  *  
  */
 const express = require('express');
@@ -7,9 +8,11 @@ const app = express();
 
 //* Require module, use middleware:
 const session = require('express-session');
-app.use(session({secret: 'secretKey'}));
+const { use } = require('express/lib/application');
+const sessionOptions = { secret: 'secretKey', resave: false, saveUninitialized: false }; //? Options to rid of depracation warnings.
+app.use(session(sessionOptions));
 
-
+//* Session Route tip - adding a property to the session object to reuse on each visit
 app.get('/viewcount', (req, res) => {
     if(req.session.count){
         req.session.count += 1;
@@ -19,6 +22,20 @@ app.get('/viewcount', (req, res) => {
     res.send(`Your View Count, Sir? \n It's ${req.session.count}`);
 });
 
+//* Save Query A:
+//* 1 - Get Query String (default if not there)
+//* 2 - Add to sessions object
+app.get('/register', (req, res) => {
+    const { username = 'Ricky Bobby' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+});
+//* Save Query B:
+//* 3 - Deconstruct from the sessions object
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome Back, ${ username }`);
+});
 
 app.listen(3000, () => {
     console.log("Serving From Port 3000");
