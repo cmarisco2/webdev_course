a6f24d1ef44679ebb34e1667ca4ac075575d7583
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-24 14:29:31 -0700

    require and run middleware on express-session. on login and register routes, after saving to the db, mutates the req.sessions object to have a user id field equal to the user._id field from the database. Upon accessing secret route, check for existence of the user_id field and redirect if it is not present

--- 2021_Portfolio_Builder/Auth_Play/index.js
+++ 2021_Portfolio_Builder/Auth_Play/index.js
@@ -8,6 +8,8 @@ const User = require('./models/user');
 const path = require('path');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +24,7 @@ main()
 //*
 
 app.use(express.urlencoded({extended:true}));
+app.use(session({secret: 'secretCode'}));
 
 
 //* Gets Form To Register User
@@ -33,6 +36,7 @@ app.post('/register', async (req, res) => {
     const hash = await bcrypt.hash(password, 12);
     const user = new User({username, password: hash});
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/');
 });
 
@@ -45,13 +49,18 @@ app.post('/login', async (req, res) => {
     const user = await User.findOne({ username }); //? This is why usernames need be unique
     const validPassword = await bcrypt.compare(password, user.password);
     if(!user || !validPassword) res.send('Incorrect Username Or Password');
-    if(validPassword) res.send("Yay! Welcome Back!")
+    if(validPassword) {
+        req.session.user_id = user._id;
+        res.redirect('/secret');
+    }
+
 });
 
 app.get('/', (req, res) => {
     res.send('Home Page');
 });
 app.get('/secret', (req, res) => {
+    if(!req.session.user_id) return res.redirect('/login');
     res.send("This Path is Secret");
 });
 
