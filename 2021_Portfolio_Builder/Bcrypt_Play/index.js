bcd0d99c6ebd22c4bc3156f28c836f17dc171d19
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-23 18:45:58 -0700

    add comments for code explanations

--- 2021_Portfolio_Builder/Bcrypt_Play/index.js
+++ 2021_Portfolio_Builder/Bcrypt_Play/index.js
@@ -1,16 +1,23 @@
+/**
+ ** Demo bcrypt library for cryptographic hashing functions for passwords
+ *
+ */
 const bcrypt = require('bcrypt');
 
 
-
+//* Generate hash (salt then hash)
 const hashPassword = async(pw) => {
     const salt = await bcrypt.genSalt(12);
     const hash = await bcrypt.hash(pw, salt);
     console.log(salt);
     console.log(hash);
 }
+//! can instead do both in 1 step: 
+//! const hash = await bcrypt.hash(pw, 12)
 
-hashPassword('password123'); //result stored in db
+hashPassword('password123'); //? result stored in db
 
+//* Verify the Password
 const login = async (pw, hashedPW) => {
     const result = await bcrypt.compare(pw, hashedPW);
     if(result) return console.log("Successful Login");
