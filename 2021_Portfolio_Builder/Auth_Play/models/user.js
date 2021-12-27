471713d45759da762509ee89d4231ed85b0db0f8
Christopher Marisco <christopher.marisco@gmail.com>
2021-12-27 15:55:32 -0700

    add to user.js model of Auth_Play/ dir, middleware which runs 'pre' save. this middleware will check if password is modified and hash it if it is

--- 2021_Portfolio_Builder/Auth_Play/models/user.js
+++ 2021_Portfolio_Builder/Auth_Play/models/user.js
@@ -20,4 +20,16 @@ userSchema.statics.findAndValidate = async function (username, password) {
     return isValid ? foundUser : false;
 }
 
+//! Add middleware to hash passwords with model vice the index.js
+//! 1) pre/post
+//! 2) pass next as param
+//! 3) call next() at the end 
+//* ex: userSchema.pre('save', function(next)) ... //commands ... next()
+//! NOT STATIC -> 'this' is for the INSTANCE
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
 module.exports = mongoose.model('User', userSchema);
