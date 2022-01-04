56df44e9fb35601f116086e0f4c284a151e4ac6c
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-04 12:08:58 -0700

    add to the middleware.js file in Yelp_Camp/ to the isLoggedIn middleware function a field onto the req.session object that stores the req.originalUrl path. Use this path in the register route for returning to where the user was intending to go prior to registering/logging in

--- 2021_Portfolio_Builder/Yelp_Camp/middleware.js
+++ 2021_Portfolio_Builder/Yelp_Camp/middleware.js
@@ -1,5 +1,6 @@
 const isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'Must Be Logged In to Create New Campground');
         return res.redirect('/login');
     }
