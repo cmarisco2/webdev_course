da290f064f552a3309fb03ab2ce1e3fe84aa40b7
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-05 15:08:38 -0700

    Comment Out the Author Portion of the Joi validation schema

--- 2021_Portfolio_Builder/Yelp_Camp/validationSchemas/schemas.js
+++ 2021_Portfolio_Builder/Yelp_Camp/validationSchemas/schemas.js
@@ -11,8 +11,8 @@ module.exports.campgroundSchema = Joi.object({
         price: Joi.number().required().min(0),
         image: Joi.string().required(),
         description: Joi.string().required(),
-        location: Joi.string().required(),
-        author: Joi.string().required()
+        location: Joi.string().required()
+        // author: Joi.string().required()
     }).required()
 });
 
