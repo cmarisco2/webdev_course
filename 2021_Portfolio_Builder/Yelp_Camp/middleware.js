c645a24cd2ee2feb4f3ee764ca83717a6a22f0b3
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-06 14:43:09 -0700

    Change the isLoggedIn middleware function of middleware.js in Yelp_Camp/ to check the req.query object for and method headers. if so, set the session.returnTo property to redirect to the show page. if not, redirect to req.originalUrl. This solves the problem of redirecting to non-GET routes if user needs to login and be rerouted.

--- 2021_Portfolio_Builder/Yelp_Camp/middleware.js
+++ 2021_Portfolio_Builder/Yelp_Camp/middleware.js
@@ -1,4 +1,5 @@
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const ExpressError = require('./utilities/ExpressError');
 const { campgroundSchema, reviewSchema } = require('./validationSchemas/schemas');
 
@@ -15,7 +16,8 @@ const validateCampground = (req, res, next) => {
 
 const isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
-        req.session.returnTo = req.originalUrl;
+        const { id } = req.params;
+        req.session.returnTo = (!req.query._method ? req.originalUrl : `/campgrounds/${id}`)
         req.flash('error', 'Must Be Logged In To Perform This Action');
         return res.redirect('/login');
     }
@@ -34,6 +36,17 @@ const isAuthorCampground = async (req, res, next) => {
     next();
 }
 
+//* Middleware for checking valid author of review prior to deleting:
+const isAuthorReview = async (req, res, next) => {
+    const { reviewId, id } = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
+        req.flash('error', 'This Action is Permitted Only By the Review Author');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
 //* Middleware for validating reviews.
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
@@ -47,5 +60,6 @@ const validateReview = (req, res, next) => {
 
 module.exports.isLoggedIn = isLoggedIn;
 module.exports.isAuthorCampground = isAuthorCampground;
+module.exports.isAuthorReview = isAuthorReview;
 module.exports.validateCampground = validateCampground;
 module.exports.validateReview = validateReview;
