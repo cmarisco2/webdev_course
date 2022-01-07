69c532cd2fd65ea748f131d982d071950ee8e9e7
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-06 18:57:20 -0700

    Remove excess route processing logic from routes/user.js and replace with function calls from the user controller.

--- 2021_Portfolio_Builder/Yelp_Camp/routes/user.js
+++ 2021_Portfolio_Builder/Yelp_Camp/routes/user.js
@@ -9,53 +9,21 @@ const catchAsync = require('../utilities/catchAsync');
 const ExpressError = require('../utilities/ExpressError');
 const passport = require('passport');
 const User = require('../models/user');
+const user = require('../controllers/user');
 
 
 //* Create New User: 1) GET FORM 2) POST FORM
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
-
-router.post('/register', catchAsync(async(req, res, next) => {
-    try {
-
-        const { username, email, password } = req.body;
-        const user = new User({username, email});
-        const registeredUser = await User.register(user, password);
-        //* logs in after register. Need callback to handle error (no await supported).
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success' ,'Welcome To Yelp Camp!');
-            res.redirect('/campgrounds');
-        });
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}));
+router.get('/register', user.registrationForm);
+router.post('/register', catchAsync(user.createUser));
 
 //* Login as an already registered user:
-router.get('/login', (req, res) => {
-    res.render('users/login');
-});
+router.get('/login', user.loginForm);
 
 //! Passport Middleware to login -> passport.authenticate()
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}) ,(req, res) => {
-    req.flash('success', 'Welcome Back!');
-    //* Shorts to session info, if there, else to default.
-    const redirectedUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectedUrl);
-});
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}) , user.loginUser);
 
 //* Logout Route: use req.logout() then redirect
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Logged Out -> See You Next Time');
-    res.redirect('/campgrounds');
-});
-
-
+router.get('/logout', user.logoutUser);
 
 
 module.exports = router;
