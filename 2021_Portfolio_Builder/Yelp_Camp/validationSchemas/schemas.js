a1c0920e0b7a236531dad8c796b20a4971c7c084
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-09 17:48:37 -0700

    update validationSchemas/schemas.js within Yelp_Camp/ to validate 'images' vice 'image' as an array of objects.

--- 2021_Portfolio_Builder/Yelp_Camp/validationSchemas/schemas.js
+++ 2021_Portfolio_Builder/Yelp_Camp/validationSchemas/schemas.js
@@ -9,7 +9,10 @@ module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().required(),
+        images: Joi.array().items({
+            url: Joi.string().required(),
+            filename: Joi.string().required()
+        }),
         description: Joi.string().required(),
         location: Joi.string().required()
         // author: Joi.string().required()
