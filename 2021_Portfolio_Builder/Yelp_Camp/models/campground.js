f1471f3eeb31687a131a26684550c35b08b0bde1
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-09 17:47:39 -0700

    Change the models/campground.js image propery to 'images'. Set the property as an array of objects with 'url: string' and 'filename: string' in the Yelp_Camp/ project

--- 2021_Portfolio_Builder/Yelp_Camp/models/campground.js
+++ 2021_Portfolio_Builder/Yelp_Camp/models/campground.js
@@ -6,7 +6,12 @@ const Review = require('./review');
 
 const campgroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [
+        {
+            url: String,
+            filename: String
+        }
+    ],
     price: Number,
     description: String,
     location: String,
