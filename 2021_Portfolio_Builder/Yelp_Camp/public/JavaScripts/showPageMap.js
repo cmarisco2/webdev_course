e07209c825f2417c24d7cf2989d716b5d6e0d958
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-10 18:55:25 -0700

    fix geometry display and add popups to the public/JavaScripts/showPageMap.js file in Yelp_Camp/

--- 2021_Portfolio_Builder/Yelp_Camp/public/JavaScripts/showPageMap.js
+++ 2021_Portfolio_Builder/Yelp_Camp/public/JavaScripts/showPageMap.js
@@ -1,5 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/light-v10', //stylesheet location
+    center: campground.geometry.coordinates,
+    zoom: 9
 });
+
+// Create a new marker.
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates) // [lng, lat]
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+                ))
+    .addTo(map);
