91cf0c97fff4cc7fce2f3300cd26a346a5479df6
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-16 11:31:20 -0700

    fix seeds/index of yelp-camp to randomize the images shown on the index page

--- 2021_Portfolio_Builder/Yelp_Camp/seeds/index.js
+++ 2021_Portfolio_Builder/Yelp_Camp/seeds/index.js
@@ -4,11 +4,15 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
+const photos = require('./photos');
 const { descriptors, places } = require('./seedHelper');
+//* Production vs Local
+// const db_url = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const db_url = process.env.DB_URL;
 
 //* Connection Boilerplate w/ yelp-camp as the end-point
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+    await mongoose.connect(db_url);
 }
 main()
     .then(() => console.log('connected to Yelp-Camp DB'))
@@ -21,20 +25,16 @@ const seedDB = async () => {
     for(let i = 0; i < 300; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 50) + 10;
+        const imageUrl1 = arraySelector(photos, 41);
+        const filename1 = imageUrl1.slice(62, 91);
         await new Campground({
             title: `${arraySampler(descriptors)}, ${arraySampler(places)}`,
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
             images: [
                 { 
-                    url: "https://res.cloudinary.com/dmlzjpicq/image/upload/v1641846714/YelpCamp/auvje3n7mfc89soyvdsc.jpg", 
+                    url: imageUrl1, 
                 
-                    filename: "YelpCamp/auvje3n7mfc89soyvdsc"
-                },
-                
-                {
-                    url: "https://res.cloudinary.com/dmlzjpicq/image/upload/v1641920870/YelpCamp/yqwqvweeytjqpcailpmf.jpg", 
-                
-                    filename: "YelpCamp/yqwqvweeytjqpcailpmf"
+                    filename: filename1
                 }
             ],
             geometry: {
@@ -55,6 +55,8 @@ const seedDB = async () => {
 
 //*Returns Random element of an array -> Math.random() * array.length, floored, as the index
 const arraySampler = (array) => array[Math.floor(Math.random() * array.length)];
+//*Sampler with a prime modulus
+const arraySelector = (array, prime) => array[Math.floor(Math.random() * array.length) % prime];
 
 
 //?Runs seeding, then closes the connection.
